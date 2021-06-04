c410f29187cc11f6616145966b55c89aa53279fe
Min Latt <minminlaxz@gmail.com>
2021-06-04 20:50:37 +0630

    PR fixed - marged conflict

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-import './components/EmailRequest/bootstrap.min.css';
-import EmailRequest from './components/EmailRequest/EmailRequest';
-
-function App() {
-  return (
-    <div className="container">
-     <h1>Alpha Learn</h1>
-     <EmailRequest/>
-    </div>
-=======
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import { Home } from './components/Home/Home';
 
@@ -20,7 +9,6 @@ function App() {
        <Route exact path='/' component={Home} />
      </Switch>
    </Router>
->>>>>>> development
   );
 }
 
