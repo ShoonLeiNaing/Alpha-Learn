82d0ec68ce7885cf4df23de27c74f3eb90938a45
ShoonLei <pandapanda254.pp@gmail.com>
2021-06-27 15:48:32 +0630

    video upper part done

---
+++ src/pages/CourseVideo/lessons.js
@@ -0,0 +1,41 @@
+export const lessons =[
+    {
+        id:1,
+        title:'Introduction to React',
+        state:1,
+        duration:'16:35'
+    },
+    {
+        id:2,
+        title:'Components and life cycles',
+        state:1,
+        duration:'22:46'
+    },
+    {
+        id:3,
+        title:'Functional Components',
+        state:1,
+        duration:'19:12'
+    },
+    {
+        id:4,
+        title:'Introduction to Redux Part 1',
+        state:2,
+        duration:'37:00'
+    },
+    {
+        id:5,
+        title:'Introduction to Redux Part 2',
+        state:0,
+        duration:'35:07'
+    },
+    {
+        id:6,
+        title:'Project using react and redux',
+        state:0,
+        duration:'14:43'
+    },
+    
+    
+    
+]
